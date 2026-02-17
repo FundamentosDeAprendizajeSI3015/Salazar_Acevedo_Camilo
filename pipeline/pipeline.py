9c408f3bbb12c66ac947492abf046625eb0049d3
CamiloSAcevedo <142268173+CamiloSAcevedo@users.noreply.github.com>
2026-02-17 10:48:21 -0500

    Enhance EDA with visualizations and statistics
    Added data visualization and statistical analysis for EDA.

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
 
@@ -30,7 +33,7 @@ df = df[
 
 df = df.dropna(subset=["listed_in"])
 
-df["imdb_score"] = df["imdb_score"].fillna(df["imdb_score"].median)
+df["imdb_score"] = df["imdb_score"].fillna(df["imdb_score"].median())
 df["imdb_votes"] = df["imdb_votes"].fillna(0)
 df["country"] = df["country"].fillna("unkown")
 df["age_certification"] = df["age_certification"].fillna("unknown")
@@ -64,7 +67,7 @@ numeric_cols = [
 ]
 
 categorical_cols = [
-    "type",
+    "type_base",
     "rating",
     "age_certification",
     "country"
@@ -76,4 +79,72 @@ preprocessor = ColumnTransformer(
         ("cat", OneHotEncoder(handle_unknown="ignore"), categorical_cols)
     ]
 )
+
+
+#diagramas
+os.makedirs("eda_outputs", exist_ok=True)
+
+stats = pd.DataFrame()
+
+stats["mean"] = df[numeric_cols].mean()
+stats["median"] = df[numeric_cols].median()
+stats["std"] = df[numeric_cols].std()
+stats["var"] = df[numeric_cols].var()
+stats["min"] = df[numeric_cols].min()
+stats["25%"] = df[numeric_cols].quantile(0.25)
+stats["50%"] = df[numeric_cols].quantile(0.50)
+stats["75%"] = df[numeric_cols].quantile(0.75)
+stats["max"] = df[numeric_cols].max()
+
+stats.to_csv("stadistical_summary.csv")
+
+outliers_summary = {}
+
+for col in numeric_cols:
+    Q1 = df[col].quantile(0.25)
+    Q3 = df[col].quantile(0.75)
+    IQR = Q3 - Q1
+
+    lower_bound = Q1 - 1.5 * IQR
+    upper_bound = Q3 + 1.5 * IQR
+
+    outliers = df[(df[col] < lower_bound) | (df[col] > upper_bound)]
+
+    outliers_summary[col] = len(outliers)
+
+pd.DataFrame.from_dict(outliers_summary, orient="index", columns=["n_outliers"])\
+    .to_csv("outliers_summary.csv")
+
+#histogramas
+
+for col in numeric_cols:
+    plt.figure()
+    sns.histplot(df[col], kde=True)
+    plt.title(f"Hisotgrama de {col}")
+    plt.savefig(f"eda_outputs/hist_{col}.png")
+    plt.close()
+
+#gráficos de dispersión
+
+pairs = [
+    ("imdb_score", "imdb_votes"),
+    ("release_year", "imdb_score"),
+    ("duration_num", "imdb_score"),
+]
+
+for x, y in pairs:
+    plt.figure()
+    sns.scatterplot(x=df[x], y=df[y])
+    plt.title(f"{x} vs {y}")
+    plt.savefig(f"eda_outputs/scatter_{x}_vs{y}.png")
+
+    plt.close()
+
+plt.figure()
+corr_matrix = df[numeric_cols].corr()
+
+sns.heatmap(corr_matrix, annot=True, cmap="coolwarm", fmt=".2f")
+plt.title("matriz de correlación")
+plt.savefig("eda_outputs/correlation_matrix.png")
+plt.close()
 
